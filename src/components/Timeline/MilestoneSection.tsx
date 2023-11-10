49a258241ee53e8058141cb159ce6d58597ca4f8
arifm6 <arifmassih6@gmail.com>
2023-11-10 16:24:09 -0500

    Updated Desktop Timeline Animation

--- src/components/Timeline/MilestoneSection.tsx
+++ src/components/Timeline/MilestoneSection.tsx
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import type { ProcessedMilestone } from "./Timeline";
 import MilestoneDescription from "./MilestoneDescription";
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 type MilestoneSectionProps = {
   milestone: ProcessedMilestone;
   index: number;
   mainColor: string;
   buttonColor: string;
+  animate: boolean;
+  onAnimationComplete: () => void;
 };
 
 export default function MilestoneSection({
@@ -15,18 +17,54 @@ export default function MilestoneSection({
   index,
   mainColor,
   buttonColor,
+  animate,
+  onAnimationComplete,
 }: MilestoneSectionProps) {
   const alternate = index % 2 === 0;
   const [dateIsHovered, setDateIsHovered] = useState(false);
   const initialDateProps = { width: 0, opacity: 0 };
   const animateDateProps = { width: "auto", opacity: 1 };
+  const leftLineControls = useAnimation();
+  const dateAndButtonControls = useAnimation();
+  const milestoneDescriptionControls = useAnimation();
+  const rightLineControls = useAnimation();
+  useEffect(() => {
+    const animateSection = async () => {
+      // Your animation logic for the first element
+      await leftLineControls.start({ width: "100%" });
+
+      // Animation for the date elements
+      await dateAndButtonControls.start({ opacity: 1 });
+
+      //Animation for Milestone Description Section
+      await milestoneDescriptionControls.start("visible");
+      await rightLineControls.start({ width: "100%", opacity: 1 });
+
+      // Animation for the next element, and so on...
+      // You can add more animations here
+
+      // Repeat the process for other animations
+
+      // Callback to notify the parent that animation is complete
+      onAnimationComplete();
+    };
+
+    if (animate) {
+      animateSection();
+    }
+  }, [animate]);
 
   return (
     <div className="flex justify-center items-center grow relative ">
-      <div
-        style={{ backgroundColor: mainColor }}
-        className={`grow h-[1px] bg-[${mainColor}]`}
-      ></div>
+      <div className="grow">
+        <motion.div
+          initial={{ width: 0 }}
+          animate={leftLineControls}
+          transition={{ duration: 1 }}
+          style={{ backgroundColor: mainColor }}
+          className="grow h-[1px]"
+        ></motion.div>
+      </div>
       <div
         onMouseEnter={() => setDateIsHovered(true)}
         onMouseLeave={() => {
@@ -37,7 +75,11 @@ export default function MilestoneSection({
           alternate ? "bottom-full" : "top-full"
         } text-xl font-bold my-2`}
       >
-        <div className="flex">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={dateAndButtonControls}
+          className="flex"
+        >
           <span>{milestone.startDateString}</span>
           <motion.span
             initial={dateIsHovered ? animateDateProps : initialDateProps}
@@ -46,9 +88,12 @@ export default function MilestoneSection({
           >
             &nbsp;-&nbsp;{milestone.endDateString}
           </motion.span>
-        </div>
+        </motion.div>
       </div>
-      <div
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={dateAndButtonControls}
+        transition={{ duration: 1 }}
         style={{ borderColor: buttonColor }}
         className="relative w-8 h-8 border-2 rounded-full"
       >
@@ -56,9 +101,22 @@ export default function MilestoneSection({
           style={{ backgroundColor: buttonColor }}
           className="w-4 h-4 absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 rounded-full"
         ></div>
-      </div>
+      </motion.div>
 
-      <div
+      <motion.div
+        initial="hidden"
+        animate={milestoneDescriptionControls}
+        variants={{
+          hidden: {
+            y: "0",
+            opacity: 0,
+          },
+          visible: {
+            y: alternate ? "8px" : "-8px",
+            opacity: 1,
+          },
+        }}
+        transition={{ duration: 1 }}
         className={`absolute ${
           alternate
             ? "top-full flex-col translate-y-2"
@@ -73,11 +131,17 @@ export default function MilestoneSection({
             position={milestone.position}
           />
         </div>
+      </motion.div>
+
+      <div className="grow">
+        <motion.div
+          initial={{ width: 0, opacity: 0 }}
+          animate={rightLineControls}
+          transition={{ duration: 1 }}
+          style={{ backgroundColor: mainColor }}
+          className="grow h-[1px]"
+        ></motion.div>
       </div>
-      <div
-        style={{ backgroundColor: mainColor }}
-        className="grow h-[1px] "
-      ></div>
     </div>
   );
 }
--- src/components/Timeline/TimelineDesktop.tsx
+++ src/components/Timeline/TimelineDesktop.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import MilestoneDescription from "./MilestoneDescription";
 import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
@@ -15,7 +15,18 @@ export default function TimelineDesktop({
   mainColor,
   buttonColor,
 }: TimelineDesktopProps) {
-  //   const mainColorTextStyle = { color: mainColor };
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const onAnimationComplete = () => {
+    setCurrentIndex((prevIndex) => {
+      const nextIndex = prevIndex + 1;
+      if (nextIndex < milestones.length) {
+        return nextIndex;
+      }
+      return prevIndex;
+    });
+  };
+
   return (
     <div className="w-full flex">
       {milestones.map((milestone, index) => {
@@ -26,93 +37,11 @@ export default function TimelineDesktop({
             index={index}
             mainColor={mainColor}
             buttonColor={buttonColor}
+            animate={index === currentIndex}
+            onAnimationComplete={onAnimationComplete}
           />
         );
       })}
     </div>
-    // <div className="block w-full relative">
-    //   <div className="flex w-full items-center">
-    //     <div
-    //       style={{ backgroundColor: mainColor }}
-    //       className={`grow-[0.5] h-[1px] bg-[${mainColor}]`}
-    //     ></div>
-    //     {timelineMilestones.map((milestone, index) => {
-    //       //alternate arrow each time
-    //       const alternate = index % 2 === 0;
-    //       return (
-    //         <div className="relative flex items-center grow " key={index}>
-    //           <div
-    //             style={{ borderColor: buttonColor }}
-    //             className={`h-8 w-8 border-[2px] rounded-full relative`}
-    //           >
-    //             <div
-    //               className="absolute  inset-0 rounded-full cursor-pointer"
-    //               onClick={() => {
-    //                 milestone.redirectUrl && router.push(milestone.redirectUrl);
-    //               }}
-    //             >
-    //               <div
-    //                 style={{ backgroundColor: buttonColor }}
-    //                 className={`h-4 w-4 absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 rounded-full`}
-    //               ></div>
-    //             </div>
-    //             <div
-    //               className={`absolute flex  left-1/2  -translate-x-1/2  ${
-    //                 alternate
-    //                   ? "bottom-full -translate-y-2"
-    //                   : "top-full translate-y-2"
-    //               }`}
-    //             >
-    //               <div
-    //                 style={mainColorTextStyle}
-    //                 className={`flex flex-nowrap group items-center   font-bold text-xl`}
-    //                 onMouseEnter={() => setDateIsHovered(true)}
-    //                 onMouseLeave={() => {
-    //                   setDateIsHovered(false);
-    //                 }}
-    //               >
-    //                 <span className="whitespace-nowrap">
-    //                   {milestone.startDateString}
-    //                 </span>
-    //                 <motion.span
-    //                   initial={
-    //                     dateIsHovered ? animateDateProps : initialDateProps
-    //                   }
-    //                   animate={
-    //                     dateIsHovered ? animateDateProps : initialDateProps
-    //                   }
-    //                   className="flex whitespace-nowrap"
-    //                 >
-    //                   &nbsp;- {milestone.endDateString}
-    //                 </motion.span>
-    //               </div>
-    //             </div>
-
-    //             <div
-    //               className={`flex items-center w-full absolute ${
-    //                 alternate
-    //                   ? "flex-col top-full translate-y-2 "
-    //                   : "flex-col-reverse bottom-full -translate-y-2"
-    //               }`}
-    //             >
-    //               <div className="w-[1px] h-[49px] bg-gray-400"></div>
-    //               <div className="w-1 h-1 bg-gray-400 rounded-full"></div>
-    //               <MilestoneDescription
-    //                 company={milestone.company}
-    //                 position={milestone.position}
-    //                 startDate={milestone.startDateString}
-    //                 endDate={milestone.endDateString}
-    //               />
-    //             </div>
-    //           </div>
-    //           <div
-    //             style={{ backgroundColor: mainColor }}
-    //             className={`h-[1px] grow bg-[${mainColor}]`}
-    //           ></div>
-    //         </div>
-    //       );
-    //     })}
-    //   </div>
-    // </div>
   );
 }
